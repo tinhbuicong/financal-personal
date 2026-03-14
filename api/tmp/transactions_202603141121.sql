e435ec1e687b5003f00ab0f021868aaef3c79bc1
eren <tinhbuicong25@gmail.com>
2026-03-14 11:26:48 +0700

    1

---
+++ api/tmp/transactions_202603141121.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.transactions (amount,"type",category,note,"date") VALUES
+	 (102,'outcome','Ăn tối','','2026-03-06'),
+	 (80,'outcome','Xăng','','2026-03-08'),
+	 (95,'outcome','Hộp cơm','','2026-03-09'),
+	 (281,'outcome','Gà texas','','2026-03-09'),
+	 (139,'outcome','Thẻ wifi','','2026-03-09');
